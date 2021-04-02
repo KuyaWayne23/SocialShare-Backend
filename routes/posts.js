d91a01b52e502af61797d532705f12f32be791ef
Orlie Wayne A. Faustorilla <orliewayne.faustorilla@gmail.com>
2021-04-02 16:20:20 +0800

    added find post by user feature

--- routes/posts.js
+++ routes/posts.js
@@ -1,27 +1,41 @@
 const router = require('express').Router();
 const Authorization = require('../authorization');
 const Post = require('../models/Posts');
+const User = require('../models/Users');
 
 router.get('/', Authorization.validate, (request, response, next) => {
-    response.send(`Get all posts here`);
+    Post.find({})
+    .populate('author', ['firstname', 'middlename', 'lastname'])
+    .exec()
+    .then((result) => {
+        response.send(result);
+    }).catch(next);
 });
 
 router.get('/author/:UserID', Authorization.validate, (request, response, next) => {
-    response.send(`Get all posts from user`);
+    const {UserID} = request.params;
+
+    Post.find({author: UserID})
+    .populate('author', ['firstname', 'middlename', 'lastname'])
+    .exec()
+    .then((result) => {
+        response.send(result);
+    })
+    .catch(next)
 });
 
 router.get('/:PostID', Authorization.validate, (request, response, next) => {
     response.send(`Get One Post`);
 });
 
-router.post('/create', Authorization.validate, (request, response, next) => {
+router.post('/create', Authorization.validate, async (request, response, next) => {
     const {author, message} = request.body;
-
-    Post.create({ author, message })
+    const PostID = await Post.create({ author, message }).then((result) => result._id).catch(next);
+    
+    User.findByIdAndUpdate(author, {$push: {posts: PostID}}, {new: true})
     .then((result) => {
-        response.send(result);
-    })
-    .catch(next);
+        response.send(true);
+    }).catch(next);
 });
 
 router.patch('/update/:PostID', Authorization.validate, (request, response, next) => {
