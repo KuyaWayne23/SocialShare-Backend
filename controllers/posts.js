20ab3e4be6cb0e3fbd107cf17e03aa734734269a
Orlie Wayne A. Faustorilla <orliewayne.faustorilla@gmail.com>
2021-04-02 17:03:38 +0800

    added update post

---
+++ controllers/posts.js
@@ -0,0 +1,69 @@
+const Post = require('../models/Posts');
+const User = require('../models/Users');
+
+module.exports.getAll = (request, response, next) => {
+    Post.find({})
+    .populate('author', ['firstname', 'middlename', 'lastname'])
+    .exec()
+    .then((result) => {
+        response.send(result);
+    }).catch(next);
+};
+
+module.exports.getByAuthor = async (request, response, next) => {
+    const {UserID} = request.params;
+
+    const Posts = await Post.find({author: UserID})
+    .populate('author', ['firstname', 'middlename', 'lastname'])
+    .exec()
+    .then((result) => result)
+    .catch(next);
+
+    response.send(Posts);
+};
+
+module.exports.getSpecific = (request, response, next) => {
+    const {PostID} = request.params;
+
+    Post.findById(PostID)
+    .populate('author', ['firstname', 'middlename', 'lastname'])
+    .exec()
+    .then((result) => {
+        response.send(result);    
+    }).catch(next);
+};
+
+module.exports.createPost = async (request, response, next) => {
+    const {author, message} = request.body;
+    const PostID = await Post.create({ author, message }).then((result) => result._id).catch(next);
+    
+    const PostsByUser =  await User.findByIdAndUpdate(author, {$push: {posts: PostID}}, {new: true})
+    .then((result) => true)
+    .catch(next);
+
+    response.send(PostsByUser);
+};
+
+module.exports.updatePost = async (request, response, next) => {
+    const {PostID} = await request.params;
+    const {message} = await request.body;
+
+    const updated = await Post.findByIdAndUpdate(PostID, {
+        $set: {
+            message,
+            date_posted: new Date()
+        }
+    })
+    .then(result => true)
+    .catch(error => false)
+
+    await response.send(updated)
+};
+
+module.exports.deletePost = (request, response, next) => {
+    const {PostID} = request.params;
+    
+    Post.findByIdAndDelete(PostID)
+    .then(result => true)
+    .catch(error => false)
+};
--- routes/posts.js
+++ routes/posts.js
@@ -1,57 +1,17 @@
 const router = require('express').Router();
 const Authorization = require('../authorization');
-const Post = require('../models/Posts');
-const User = require('../models/Users');
+const PostController = require('../controllers/posts');
 
-router.get('/', Authorization.validate, (request, response, next) => {
-    Post.find({})
-    .populate('author', ['firstname', 'middlename', 'lastname'])
-    .exec()
-    .then((result) => {
-        response.send(result);
-    }).catch(next);
-});
+router.get('/', Authorization.validate, PostController.getAll);
 
-router.get('/author/:UserID', Authorization.validate, async (request, response, next) => {
-    const {UserID} = request.params;
+router.get('/author/:UserID', Authorization.validate, PostController.getByAuthor);
 
-    const Posts = await Post.find({author: UserID})
-    .populate('author', ['firstname', 'middlename', 'lastname'])
-    .exec()
-    .then((result) => result)
-    .catch(next);
+router.get('/:PostID', Authorization.validate, PostController.getSpecific);
 
-    response.send(Posts);
-});
+router.post('/create', Authorization.validate, PostController.createPost);
 
-router.get('/:PostID', Authorization.validate, (request, response, next) => {
-    const {PostID} = request.params;
+router.patch('/update/:PostID', Authorization.validate, PostController.updatePost);
 
-    Post.findById(PostID)
-    .populate('author', ['firstname', 'middlename', 'lastname'])
-    .exec()
-    .then((result) => {
-        response.send(result);    
-    }).catch(next);
-});
-
-router.post('/create', Authorization.validate, async (request, response, next) => {
-    const {author, message} = request.body;
-    const PostID = await Post.create({ author, message }).then((result) => result._id).catch(next);
-    
-    const PostsByUser =  await User.findByIdAndUpdate(author, {$push: {posts: PostID}}, {new: true})
-    .then((result) => true)
-    .catch(next);
-
-    response.send(PostsByUser);
-});
-
-router.patch('/update/:PostID', Authorization.validate, (request, response, next) => {
-    response.send('Update Post Here');
-});
-
-router.delete('/delete/:PostID', Authorization.validate, (request, response, next) => {
-    response.send('Delete Post Here');
-});
+router.delete('/delete/:PostID', Authorization.validate, PostController.deletePost);
 
 module.exports = router;
